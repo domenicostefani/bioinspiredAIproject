0c5f7358aff3fae3ec36f07b144e7ffb8a8ce5cd
dodostefani <domenico.stefani@studenti.unitn.it>
2020-01-08 12:52:42 +0100

    add custom operators (MAJOR) Custom mutators and crossover operators are added:  * Mutators     - life_flip_mutation (similar if not equal to random bitflip, may                           be more efficient because it deals with bools                           directly)     - resetrandom_mutation (resets some cells and set to 1 randomly                             with a bias argument [0,1] that states the                             probability of using a True value for the                             new cell. With low bias values it produces                             sparser configurations which is good)  * Crossover operators     - UNIONcrossover (Set UNION, Logical OR. 1 offspring)     - INTERcrossover (Set INTERSECTION, Logical AND. 1 offspring)     - XORcrossover   (Logical XOR. 1 offspring)     - SUBcrossover   (Set difference, 2 offsprings because both A-B and                       B-A are performed)
    These operators are added to adapt the evolution to the specific 
    problem. The first results seem to show an improvement in the sense 
    that now sparser configurations are explored and for the first time the
    algorithm generates a glider ship from the beginning.
    First result is added too

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import numpy as np
 import os
 import life
 from inspyred import ec
+import copy
 
 """--Parameters for LIFE..---------------------------------------------------"""
 
@@ -26,11 +27,11 @@ TARGET = (N-4,N-4)
 
 """--Parameters for the EC---------------------------------------------------"""
 
-populationSize = 30
-numberOfGenerations = 100
+populationSize = 50
+numberOfGenerations = 500
 numberOfEvaluations = 2500                    # used with evaluation_termination
 tournamentSize = 3
-mutationRate = 0.8
+mutationRate = 0.2
 gaussianMean = 0
 gaussianStdev = 10.0
 crossoverRate = 0.95
@@ -40,6 +41,8 @@ numElites = 2
 
 """--Visualization-----------------------------------------------------------"""
 display = True
+SHOW_BEFOREAFTER_LIFEFLIP = False
+SHOW_BEFOREAFTER_RESETRANDOM = False
 """--------------------------------------------------------------------------"""
 
 # this object is used for single-thread evaluations (only pickleable objects can be used in multi-thread)
@@ -62,6 +65,12 @@ class AutomatonEvaluator():
     def evaluator(self, candidates, args):
         fitness = []
         for candidate in candidates:
+            """
+            # TODO: Here we can computate also the INITIAL size and use it
+              in the fitness formulation.
+              Also the number of alive cells, maybe it's better cause on a
+              small matrix size does not vary much
+            """
             distances,sizes,iterations = life.compute_fitness(candidate,
                                                               MAX_ITERATIONS,
                                                               TARGET)
@@ -69,7 +78,18 @@ class AutomatonEvaluator():
             (final_distance,min_distance) = distances
             (final_size,max_size,avg_size) = sizes
 
+
+
             """-----------Fitness formulation--------------------------------"""
+            """
+               Metrics that can be used
+                - final_distance
+                - min_distance
+                - final_size
+                - max_size
+                - avg_size
+                - iterations
+            """
             if(final_distance != 0):
                 iterations = 1000
                 max_size = 1600
@@ -83,6 +103,223 @@ class AutomatonEvaluator():
             fitness.append(fitness_c)
         self.genCount += 1
         return fitness
+
+
+'''
+            custom CROSSOVER operators
+    Following functions implements custom crossover operators based on logic
+    operations.
+    In this application of EC, the setting is known and injecting information
+    through custom operators should be beneficial for the search.
+
+    The included set operators between automata are:
+     - union         (logical disjunction, OR)
+     - intersection  (logical conjunction, AND)
+     - subtraction   (relative complement, Both A\B and B\A)
+     - xor
+'''
+def UNIONcrossover(random, mom, dad, args):
+    """Return the union (logical disjunction) of the candidates.
+
+    ░░░░░░░░░░     ░░░░░░░░░░     ░░░░░░░░░░
+    ░░██████░░     ░░██░░░░░░     ░░██████░░
+    ░░░░░░██░░ OR  ░░██░░░░░░  =  ░░██░░██░░
+    ░░░░██░░░░     ░░██░░██░░     ░░██████░░
+    ░░░░░░░░░░     ░░░░░░░░░░     ░░░░░░░░░░
+
+    .. Arguments:
+       random -- the random number generator object
+       mom -- the first parent candidate
+       dad -- the second parent candidate
+       args -- a dictionary of keyword arguments
+
+    Optional keyword arguments in args:
+
+    - *crossover_rate* -- the rate at which crossover is performed
+      (default 1.0)
+
+    """
+    crossover_rate = args.setdefault('crossover_rate', 1.0)
+    children = []
+    if random.random() < crossover_rate:
+        son = np.logical_or(dad,mom)
+        children.append(son)
+    else:
+        children.append(mom)
+        children.append(dad)
+    return children
+
+def INTERcrossover(random, mom, dad, args):
+    """Return the intersection (logical conjunction) of the candidates.
+
+    ░░░░░░░░░░     ░░░░░░░░░░     ░░░░░░░░░░
+    ░░██████░░     ░░██░░░░░░     ░░██░░░░░░
+    ░░░░░░██░░ AND ░░██░░░░░░  =  ░░░░░░░░░░
+    ░░░░██░░░░     ░░██░░██░░     ░░░░░░░░░░
+    ░░░░░░░░░░     ░░░░░░░░░░     ░░░░░░░░░░
+
+    .. Arguments:
+       random -- the random number generator object
+       mom -- the first parent candidate
+       dad -- the second parent candidate
+       args -- a dictionary of keyword arguments
+
+    Optional keyword arguments in args:
+
+    - *crossover_rate* -- the rate at which crossover is performed
+      (default 1.0)
+
+    """
+    crossover_rate = args.setdefault('crossover_rate', 1.0)
+    children = []
+    if random.random() < crossover_rate:
+        son = np.logical_and(dad,mom)
+        children.append(son)
+    else:
+        children.append(mom)
+        children.append(dad)
+    return children
+
+def XORcrossover(random, mom, dad, args):
+    """Return the boolean XOR of the candidates.
+
+    ░░░░░░░░░░     ░░░░░░░░░░     ░░░░░░░░░░
+    ░░██████░░     ░░██░░░░░░     ░░░░████░░
+    ░░░░░░██░░ XOR ░░██░░░░░░  =  ░░██░░██░░
+    ░░░░██░░░░     ░░██░░██░░     ░░██████░░
+    ░░░░░░░░░░     ░░░░░░░░░░     ░░░░░░░░░░
+
+    .. Arguments:
+       random -- the random number generator object
+       mom -- the first parent candidate
+       dad -- the second parent candidate
+       args -- a dictionary of keyword arguments
+
+    Optional keyword arguments in args:
+
+    - *crossover_rate* -- the rate at which crossover is performed
+      (default 1.0)
+
+    """
+    crossover_rate = args.setdefault('crossover_rate', 1.0)
+    children = []
+    if random.random() < crossover_rate:
+        son = np.logical_xor(dad,mom)
+        children.append(son)
+    else:
+        children.append(mom)
+        children.append(dad)
+    return children
+
+def SUBcrossover(random, mom, dad, args):
+    """Return the two set subtractions of the candidates.
+
+    ░░░░░░░░░░     ░░░░░░░░░░     ░░░░░░░░░░
+    ░░██████░░     ░░██░░░░░░     ░░░░████░░
+    ░░░░░░██░░  \  ░░██░░░░░░  =  ░░░░░░██░░
+    ░░░░██░░░░     ░░██░░██░░     ░░░░██░░░░
+    ░░░░░░░░░░     ░░░░░░░░░░     ░░░░░░░░░░
+
+    ░░░░░░░░░░     ░░░░░░░░░░     ░░░░░░░░░░
+    ░░██░░░░░░     ░░██████░░     ░░░░░░░░░░
+    ░░██░░░░░░  \  ░░░░░░██░░  =  ░░██░░░░░░
+    ░░██░░██░░     ░░░░██░░░░     ░░██░░██░░
+    ░░░░░░░░░░     ░░░░░░░░░░     ░░░░░░░░░░
+
+    .. Arguments:
+       random -- the random number generator object
+       mom -- the first parent candidate
+       dad -- the second parent candidate
+       args -- a dictionary of keyword arguments
+
+    Optional keyword arguments in args:
+
+    - *crossover_rate* -- the rate at which crossover is performed
+      (default 1.0)
+
+    """
+    crossover_rate = args.setdefault('crossover_rate', 1.0)
+    children = []
+    if random.random() < crossover_rate:
+        bro = np.logical_and(dad,np.logical_not(mom))
+        sis = np.logical_and(mom,np.logical_not(dad))
+        children.append(bro)
+        children.append(sis)
+    else:
+        children.append(mom)
+        children.append(dad)
+    return children
+
+'''
+            custom MUTATION operators
+    Following functions implements custom mutation operators.
+
+    The included set operators between automata are:
+     - random bitflip
+     - biased reset and random flip
+'''
+
+def life_flip_mutation(random, candidate, args):
+    """Return the mutants produced by bit-flip mutation on the candidates.
+
+    .. Arguments:
+       random -- the random number generator object
+       candidate -- the candidate solution
+       args -- a dictionary of keyword arguments
+
+    Optional keyword arguments in args:
+    - *mutation_rate* -- the rate at which mutation is performed (default 0.1)
+                         The mutation rate is applied on a bit by bit basis.
+    """
+    if(SHOW_BEFOREAFTER_LIFEFLIP):
+        print("\nBefore")
+        life.display_genotype(candidate)
+
+    rate = args.setdefault('mutation_rate', 0.1)
+    mutant = copy.copy(candidate)
+    for i, m in enumerate(mutant):
+        if random.random() < rate:
+            mutant[i] = not mutant[i]
+
+    if(SHOW_BEFOREAFTER_LIFEFLIP):
+        print("\nAfter")
+        life.display_genotype(mutant)
+    return mutant
+
+def resetrandom_mutation(random, candidate, args):
+    """Reset cells and randomly flip them on, with certain bias
+
+    .. Arguments:
+       random -- the random number generator object
+       candidate -- the candidate solution
+       args -- a dictionary of keyword arguments
+
+    Optional keyword arguments in args:
+    - *mutation_rate* -- the rate at which mutation is performed (default 0.1)
+                         The mutation rate is applied on a bit by bit basis.
+    - *flip_bias*     -- The bias towards the TRUE value
+    """
+    rate = args.setdefault('mutation_rate', 0.1)
+    bias = args.setdefault('flip_bias', 0.5)
+
+    if(SHOW_BEFOREAFTER_RESETRANDOM):
+        print("\nBefore")
+        life.display_genotype(candidate)
+
+    mutant = copy.copy(candidate)
+    for i, m in enumerate(mutant):
+        if random.random() < rate:
+            if random.random() < bias:
+                mutant[i] = True
+            else:
+                mutant[i] = False
+
+    if(SHOW_BEFOREAFTER_RESETRANDOM):
+        print("\nAfter")
+        life.display_genotype(mutant)
+
+    return mutant
+
 
 def main(rng, seed, display=False):
     problem = AutomatonEvaluator(seed)
@@ -110,13 +347,19 @@ def main(rng, seed, display=False):
     ea.selector = inspyred.ec.selectors.tournament_selection
 
     # variation operators (mutation/crossover)
-    ea.variator = [inspyred.ec.variators.gaussian_mutation,
-                   # inspyred.ec.variators.n_point_crossover,
+    ea.variator = [
+                    inspyred.ec.variators.gaussian_mutation,
+                    # inspyred.ec.variators.n_point_crossover,
                     inspyred.ec.variators.random_reset_mutation,
-                    # inspyred.ec.variators.inversion_mutation,
-                    inspyred.ec.variators.uniform_crossover
-                    # inspyred.ec.variators.partially_matched_crossover
-                    ]
+                    # # inspyred.ec.variators.inversion_mutation,
+                    # inspyred.ec.variators.uniform_crossover,
+                    # inspyred.ec.variators.partially_matched_crossover,
+                    inspyred.ec.variators.crossover(UNIONcrossover),
+                    inspyred.ec.variators.crossover(INTERcrossover),
+                    inspyred.ec.variators.crossover(XORcrossover),
+                    inspyred.ec.variators.crossover(SUBcrossover),
+                    inspyred.ec.variators.mutator(life_flip_mutation),
+                    inspyred.ec.variators.mutator(resetrandom_mutation)]
 
     # replacement operator
     #ea.replacer = inspyred.ec.replacers.truncation_replacement
@@ -154,7 +397,8 @@ def main(rng, seed, display=False):
                           crossover_rate=crossoverRate,
                           num_crossover_points=numCrossoverPoints,
                           num_selected=selectionSize,
-                          num_elites=numElites)
+                          num_elites=numElites,
+                          flip_bias = 0.2)
 
     if display:
         final_pop.sort(reverse=True)
